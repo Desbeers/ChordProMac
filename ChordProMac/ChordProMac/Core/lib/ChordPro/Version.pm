7232fa1197b4ac1623e5de4439f08eee5ff0284f
Nick Berendsen <58348935+Desbeers@users.noreply.github.com>
2024-12-04 07:56:18 +0100

    Cleanup

--- ChordProMac/ChordProMac/Core/lib/ChordPro/Version.pm
+++
@@ -1,4 +0,0 @@
-# This file is generated. Do not edit!
-package ChordPro::Version;
-our $VERSION = "6.060_135";
-print "$VERSION\n" unless caller;
--- ChordProMac/ChordProMac/Core/lib/PerlIO/scalar.pm
+++
@@ -1,9 +0,0 @@
-#line 1 "<embedded>/PerlIO/scalar.pm"
-package PerlIO::scalar;
-our $VERSION = '0.31';
-require XSLoader;
-XSLoader::load();
-1;
-__END__
-
-#line 45
